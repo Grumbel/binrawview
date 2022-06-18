e70c5422301950a642e95a4bc50479daf03e7c02
Ingo Ruhnke <grumbel@gmail.com>
2022-06-18 10:49:51 +0200

    Add Nix flake

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+    flake-utils.url = "github:numtide/flake-utils";
+
+    tinycmmc.url = "github:grumbel/tinycmmc";
+    tinycmmc.inputs.nixpkgs.follows = "nixpkgs";
+    tinycmmc.inputs.flake-utils.follows = "flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, tinycmmc }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in rec {
+        packages = flake-utils.lib.flattenTree {
+          binrawview = pkgs.stdenv.mkDerivation {
+            pname = "binrawview";
+            version = "0.0.0";
+            src = nixpkgs.lib.cleanSource ./.;
+            postFixup = ''
+              wrapProgram $out/bin/binrawview \
+                --prefix LIBGL_DRIVERS_PATH ":" "${pkgs.mesa.drivers}/lib/dri" \
+                --prefix LD_LIBRARY_PATH ":" "${pkgs.mesa.drivers}/lib"
+            '';
+            nativeBuildInputs = with pkgs; [
+              cmake
+              makeWrapper
+              pkgconfig
+            ];
+            buildInputs = with pkgs; [
+              fmt
+              SDL2
+            ] ++ [
+              tinycmmc.defaultPackage.${system}
+            ];
+          };
+        };
+        defaultPackage = packages.binrawview;
+      }
+    );
+}
+
